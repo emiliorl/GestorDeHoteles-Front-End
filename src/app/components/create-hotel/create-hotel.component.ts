574c77db0b0f847a9ccebc4dd3a9b11f7908564d
jgarcia2016377 <jgarcia-2016377@kinal.edu.gt>
2021-05-22 17:55:35 -0600

    Se agrego la funcionalidad del createHote

--- src/app/components/create-hotel/create-hotel.component.ts
+++ src/app/components/create-hotel/create-hotel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hotel } from '../../models/hotel';
 import { RestHotelService } from 'src/app/services/restHotel/rest-hotel.service';
+import { RestUserService } from 'src/app/services/restUser/rest-user.service';
+
 
 @Component({
   selector: 'create-hotel',
@@ -9,15 +11,27 @@ import { RestHotelService } from 'src/app/services/restHotel/rest-hotel.service'
 })
 export class CreateHotelComponent implements OnInit {
   public hotel:Hotel;
+  public userLogg;
 
-  constructor(private hotelService : RestHotelService) { 
+  constructor(private restHotel : RestHotelService, private restUser:RestUserService) { 
     this.hotel = new Hotel('','','','','','','',null,'','',[],[]);
+    this.userLogg = this.restUser.getUser();
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
-    
+  onSubmit(statusForm){
+    this.restHotel.createHotel(this.hotel, this.userLogg._id).subscribe((res:any) => {
+      if(res.hotelSaved){
+        alert(res.message);
+        this.hotel = new Hotel('','','','','','','',null,'','',[],[]);
+        statusForm.reset();
+      }else{
+        alert(res.message);
+      }
+      
+    },
+    error => alert(error.error.message));
   }
 }
