4263e72f1ceaee8036278e568e794982d3f64dad
jgarcia2016377 <jgarcia-2016377@kinal.edu.gt>
2021-05-23 14:10:08 -0600

    Resuelto los imput de Hotel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateAdminComponent } from './components/create-admin/create-admin.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
 import { CreateHotelComponent } from './components/create-hotel/create-hotel.component';
-import { CreateServiceComponent } from './components/create-service/create-service.component';
 import { HomeComponent } from './components/home/home.component';
 import { HotelComponent } from './components/hotel/hotel.component';
 import { ListUsersComponent } from './components/list-users/list-users.component';
@@ -18,6 +17,7 @@ import { ServicesComponent } from './components/services/services.component';
 import { ListReservationComponent } from './components/list-reservation/list-reservation.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
 import { CreateReservationComponent } from './components/create-reservation/create-reservation.component';
+import { CreateServiceComponent } from './components/create-service/create-service.component';
 
 
 const routes: Routes = [
@@ -39,8 +39,8 @@ const routes: Routes = [
   {path: 'listReservation', component: ListReservationComponent},
   {path: 'reservation', component: ReservationComponent},
   {path: 'createReservation', component: CreateReservationComponent},
-  {path: 'createService', component: CreateServiceComponent},
-  {path: 'service', component: ServicesComponent}
+  {path: 'service', component: ServicesComponent},
+  {path: 'createService', component: CreateServiceComponent}
 ];
 
 @NgModule({
